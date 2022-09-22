062710480bd132d8b0f286a38dc0979ec798bbb1
Melvin Nforba <jeffnfor@gmail.com>
2022-09-21 20:43:40 -0400

    initial commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "ec2" {
   source                 = "./ec2"
   public_subnets         = module.vpc.public_subnets
   instance_type          = "t3.micro"
-  key_name               = "MyKey"
+  key_name               = "jenkins"
   user_data              = filebase64("./userdata.tpl")
   vpc_id                 = module.vpc.vpc_id
 }
